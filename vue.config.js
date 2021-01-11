dd6ad1fa838b48d2e07279dfb934f8636306495c
jerikchan <jerikchan@gmail.com>
2021-01-12 06:43:28 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/pdf/" : "",
+  chainWebpack: config => {
+    config.module
+      .rule("worker")
+      .test(/\.worker\.js$/)
+      .use("worker-loader")
+      .loader("worker-loader")
+      .end()
+  }
+};
